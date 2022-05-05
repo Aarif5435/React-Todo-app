c41760bfcea90540a8214eccabaffb7a2456b940
Aarif5435 <97459053+Aarif5435@users.noreply.github.com>
2022-05-05 17:20:43 +0530

    add alert

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,18 +1,38 @@
-import "./style.css"
+import "./style.css";
 
-export const TodoInput = ({setValue,val,data}) =>{
-
-   
-
-    return (
-        <div className="input-div">
-            
-            <input value={val} onKeyPress={(event)=>{
-                if(event.key === "Enter"){
-                    data();
-                 }
-            }}  className="input-tag" type="text" placeholder="Add a Item" onChange={(e)=>{setValue(e.target.value)}} />
-            <button onClick={data} className="btn-tag">+</button>
-        </div>
-    )
-}
+export const TodoInput = ({ setValue, val, data }) => {
+  return (
+    <div className="input-div">
+      <input
+        value={val}
+        onKeyPress={(event) => {
+          if (event.key === "Enter") {
+            if (val == "") {
+              alert("Add items please");
+            } else {
+              data();
+            }
+          }
+        }}
+        className="input-tag"
+        type="text"
+        placeholder="Add a Item"
+        onChange={(e) => {
+          setValue(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          if (val == "") {
+            alert("Add items please");
+          } else {
+            data();
+          }
+        }}
+        className="btn-tag"
+      >
+        +
+      </button>
+    </div>
+  );
+};
